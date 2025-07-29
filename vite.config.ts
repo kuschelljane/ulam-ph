9f69bc259b00768d8d392b6906d31a5df76aa552
kuschell-jane <schellaneam@gmail.com>
2025-07-29 11:12:11 +0800

    fix: update target path for api

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://recipe-backend-six.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
